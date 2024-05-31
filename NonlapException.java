98c88e1ab7956be8872e3cfb4125535b8c6ce730
Coltish <3252582845@qq.com>
2024-05-31 19:06:03 +0800

    second-commit

--- NonlapException.java
+++ NonlapException.java
@@ -7,9 +7,10 @@ public class NonlapException extends Exception{
 
     /**
      * 构造一个新的NonlapException实例。
-     * @param message 描述异常详细信息的字符串。这个信息可以通过{@link #getMessage()}方法获取。
+     * @param message 描述异常详细信息的字符串。通过{@link #getMessage()}方法获取。
      */
     public NonlapException(String message) {
         super(message);
+        
     }
 }
